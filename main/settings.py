9c5c48b1d01997b5103ca4e49bd23ef3e4208359
rachbry <rach_cutler@live.co.uk>
2024-01-05 11:56:05 +0000

    deployed to heroku

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '.recipme.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'www.recipme.com', 'recipme.com']
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
