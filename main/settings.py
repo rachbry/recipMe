1cd596bcb86911bf7463fc4417e9ee8761e9ff28
rachbry <rach_cutler@live.co.uk>
2024-01-05 11:48:54 +0000

    deployed to heroku

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com', 'recipme.com', 'www.recipme.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'recipme.com', 'www.recipme.com']
 
 
 # Application definition
